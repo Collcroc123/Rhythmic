a669dca736519d8f02c766edd8150052b3212b97
Collin Gale <Collin.g086@gmail.com>
2020-05-01 20:33:47 -0600

    First Visualizer
    Beta

--- RB Unplugged/Assets/Visualizer.cs
+++ RB Unplugged/Assets/Visualizer.cs
@@ -1,18 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Visualizer : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //https://www.youtube.com/watch?v=PgXZsoslGsg
+    public float minHeight = 15f;
+    public float maxHeight = 550f;
+    public float updateSenstivity = 0.5f;
+    public Color visualizerColor = Color.gray;
+    [Space (15)]
+    private string artistTitle;
+    private string songTitle;
+    public Text artistText;
+    public Text songText;
+    public AudioClip audioClip;
+    public bool loop = true;
+    [Space (15), Range(64, 8192)]
+    public int visualizerSimples = 64;
+    [Range(0.0f, 1.0f)]
+    public float songVolume = 1.0f;
+
+    VisualizerObjectScript[] visualizerObjects;
+    AudioSource m_audioSource;
+
     void Start()
     {
-        
+        visualizerObjects = GetComponentsInChildren<VisualizerObjectScript>();
+        if(!audioClip)
+        {
+            return;
+        }
+        m_audioSource = new GameObject ("AudioSource").AddComponent<AudioSource>();
+        m_audioSource.loop = loop;
+        m_audioSource.clip = audioClip;
+        m_audioSource.Play();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float[] spectrumData = m_audioSource.GetSpectrumData(visualizerSimples, 0, FFTWindow.BlackmanHarris);
+        for (int i = 0; i < visualizerObjects.Length; i++)
+        {
+            Vector2 newSize = visualizerObjects[i].GetComponent<RectTransform>().rect.size;
+            newSize.y = Mathf.Clamp (Mathf.Lerp (newSize.y, minHeight + (spectrumData[i] * (maxHeight - minHeight) * 5f), updateSenstivity), minHeight, maxHeight);
+            visualizerObjects[i].GetComponent<RectTransform>().sizeDelta = newSize;
+            visualizerObjects[i].GetComponent<Image>().color = visualizerColor;
+        }
+        songText.text = audioClip.name;
+        m_audioSource.volume = songVolume;
     }
 }
